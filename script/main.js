9d404efa4edf4888cb32c6a266e4695e7d0d7c4c
dumitrus <adri.dumitrescu@gmail.com>
2015-06-19 18:41:34 +0300

    canvas tag added

--- script/main.js
+++ script/main.js
@@ -1,6 +1,22 @@
 (function(){
 	function init(){
 
+		window.requestAnimFrame = (function(){
+			return  window.requestAnimationFrame       ||
+			      	window.webkitRequestAnimationFrame ||
+			      	window.mozRequestAnimationFrame    ||
+			      	function( callback ){
+			        	window.setTimeout(callback, 1000 / 60);
+			      	};
+		})();
+
+		var canvas = document.createElement("canvas"),
+			ctx = canvas.getContext("2d"),
+			cW = canvas.width = window.innerWidth,
+			cH = canvas.height = window.innerHeight;
+
+		canvas.id = "main_canvas";
+		document.body.appendChild(canvas);
 	}
 	window.addEventListener("load",init,false);
 })();
